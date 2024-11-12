fce5eb9887699f602c84c257e0edbf563cd678c1
eya46 <61458340+eya46@users.noreply.github.com>
2024-11-12 11:45:03 +0800

    弃用Sitemap, 删除无用, 修改desc
    Signed-off-by: eya46 <61458340+eya46@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,11 @@ import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
 
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://eya46.com",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind()],
   output: "server",
   adapter: node({
     mode: "standalone",

3bd4afbcfd1f17a2e82767fab98414e7742a661e
eya46 <61458340+eya46@users.noreply.github.com>
2024-11-12 12:21:59 +0800

    提供生产环境部署方式
    Signed-off-by: eya46 <61458340+eya46@users.noreply.github.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import tailwind from "@astrojs/tailwind";
 
 import node from "@astrojs/node";
 
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://eya46.com",
   integrations: [tailwind()],
   output: "server",
+  server: {
+    port: 4321,
+  },
   adapter: node({
     mode: "standalone",
   }),
